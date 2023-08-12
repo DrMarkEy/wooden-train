1baa48745747c2ebe4913d39fed90fcedcb64f2f
Marc <marcmendler@gmail.com>
2023-08-12 09:55:24 +0200

    Add midi controlled speed slider

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,11 +9,13 @@
 
     <button @click="startDriving">Go!</button>
     <button @click="stopDriving">Stop!</button>
+
+    <input class="speed-slider" type="range" v-model="speed0" min="0" max="100" :disabled="midiAvailable"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+//import HelloWorld from './components/HelloWorld.vue';
 import {COMMAND} from './Enums.js';
 //import WebSocket from 'ws';
 
@@ -21,7 +23,15 @@ import {COMMAND} from './Enums.js';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+
+  data: function() {
+    return {speed0: 0, midiAvailable: false};
+  },
+
+  mounted: function() {
+    this.$midi.setSpeedListener(0, this.setSpeed0);    
   },
 
   methods: {
@@ -32,6 +42,10 @@ export default {
 
     },
 
+    setSpeed0: function(speed) {      
+      this.speed0 = speed*100;
+    },
+
     startDriving: function() {
       this.$bluetooth.sendCommand(COMMAND.START);
     },
@@ -39,6 +53,12 @@ export default {
 
     stopDriving: function() {
       this.$bluetooth.sendCommand(COMMAND.STOP);
+    }
+  },
+
+  watch: {
+    '$midi.available': function(val) {
+      this.midiAvailable = val;
     }
   }
 }
@@ -52,5 +72,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#logo
+{
+  width: 500px;  
+}
+
+.speed-slider
+{
+  position: absolute;
+   top: 40%;
+   transform: rotate(270deg);
 }
 </style>
