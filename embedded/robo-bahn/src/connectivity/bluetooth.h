2c24668da9da2a33d69b6c944894c17d239f3c20
Marc <marcmendler@gmail.com>
2023-08-13 08:01:46 +0200

    Replace arduino BLE imports with esp native api imports

--- embedded/robo-bahn/src/connectivity/bluetooth.h
+++ embedded/robo-bahn/src/connectivity/bluetooth.h
@@ -5,13 +5,13 @@
 #ifndef bluetooth_h
 #define bluetooth_h
 
-#include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
-#include <esp_bt.h>
-#include <esp_bt_main.h>
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+
 #include <config.h>
 
 
