7026ccef53a6abc018a276366c43f5f0f4f327c5
Marc <marcmendler@gmail.com>
2024-09-22 19:59:16 +0200

    Use real soundPlayer

--- embedded/robo-bahn/src/soundplayer.h
+++ embedded/robo-bahn/src/soundplayer.h
@@ -17,63 +17,64 @@
 class SoundPlayer {
 
   private:
- ///AudioOutputI2SNoDAC *out; // TODO: Rather use AudioOutputI2S *out; if the bug with esp32 are ever fixed. Belongs to //out = new AudioOutputI2S(0, 1);
- /// AudioFileSourcePROGMEM *file = nullptr;
-  ///AudioGeneratorWAV *audioGenerator;
+  AudioOutputI2SNoDAC *out; // TODO: Rather use AudioOutputI2S *out; if the bug with esp32 are ever fixed. Belongs to //out = new AudioOutputI2S(0, 1);
+ // See: https://github.com/earlephilhower/ESP8266Audio/issues/551
+ // See: https://github.com/earlephilhower/ESP8266Audio/issues/420
+
+  AudioFileSourcePROGMEM *file = nullptr;
+  AudioGeneratorWAV *audioGenerator;
 
   public:
 
   SoundPlayer()
   {
 
-// MIGHT BE INTERESTING: MIDI-LIKE SOUND GENERATION
-/*    const char  RTTLsound[] PROGMEM = "5thSymph:d=16,o=5,b=130:g,g,g,4d#,4p,f,f,f,4d";
+    // MIGHT BE INTERESTING: MIDI-LIKE SOUND GENERATION
+    /*const char  RTTLsound[] PROGMEM = "5thSymph:d=16,o=5,b=130:g,g,g,4d#,4p,f,f,f,4d";
       file = new AudioFileSourcePROGMEM(RTTLsound, strlen_P(RTTLsound));
       flac = new AudioGeneratorRTTTL();
       rtttl->begin(file_progmem, out);*/
 
     // Configure audio sink
- ///   out = new AudioOutputI2SNoDAC();
- ///   out -> SetPinout(PIN_UNUSED_SPEAKER_ALIBI_A, PIN_UNUSED_SPEAKER_ALIBI_B, PIN_SPEAKER);
-    //out->SetGain(1.0);    // Specify Volume between 0 and 1.0
-    //out->SetChannels(1);    // Specify the channel(1) or (2)
-   // out->SetBitsPerSample(8);    //  Specify bits per sample generally 8 or 16
-    //out->SetRate(44100);      // Specify the rate here : 8000 ,22050 , 44100 , 48000
+    out = new AudioOutputI2SNoDAC();
+    out -> SetPinout(PIN_UNUSED_SPEAKER_ALIBI_A, PIN_UNUSED_SPEAKER_ALIBI_B, PIN_SPEAKER);
+    out->SetGain(1.0);    // Specify Volume between 0 and 1.0
+    out->SetChannels(1);    // Specify the channel(1) or (2)
+    out->SetBitsPerSample(8);    //  Specify bits per sample generally 8 or 16
+    out->SetRate(44100);      // Specify the rate here : 8000 ,22050 , 44100 , 48000
     //out->use_mclk = false;//(false);
     
     // Configure audio generator
-///    audioGenerator = new AudioGeneratorWAV();    
+    audioGenerator = new AudioGeneratorWAV();    
   }
 
 
   void playSound(int sound) 
   {
     // Close old sound file
- ///   if(file != nullptr) {
- ///     audioGenerator->stop();
- ///     file->close();      
- ///     delete file;
- ///   }
+    if(file != nullptr) {
+      audioGenerator->stop();
+      file->close();      
+      delete file;
+      file = nullptr;
+    }
 
     switch(sound) {
       case 1:
         
         // Configure audio source        
-   ///     file = new AudioFileSourcePROGMEM( SAMPLES_WIN, sizeof(SAMPLES_WIN) );
-   ///     audioGenerator->begin(file, out);
-tone(PIN_SPEAKER, 1440, 1000);
+        file = new AudioFileSourcePROGMEM( SAMPLES_WIN, sizeof(SAMPLES_WIN) );
+        audioGenerator->begin(file, out);
       break;
     }
   }
 
   void Loop()
   {
-   /* if (audioGenerator->isRunning()) {
+    if (audioGenerator->isRunning()) {
       if (!audioGenerator->loop()) 
         audioGenerator->stop();
-    }*/
-   tone(PIN_SPEAKER, 1440, 500);
-   sleep(1);
+    }
   }
 };
 #endif
